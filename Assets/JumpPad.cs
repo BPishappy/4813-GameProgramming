d85aae585a37617102fa6a30349dd6bc6aad9aeb
Pornkanok Pattpee <87509044+BPishappy@users.noreply.github.com>
2022-09-22 23:28:21 +0700

    Lab3

--- Assets/JumpPad.cs
+++ Assets/JumpPad.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
 public class JumpPad : MonoBehaviour
+
 {
+    [SerializeField] private Animator anim;
     private float bounce = 20F;
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -9,6 +11,7 @@ public class JumpPad : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce,ForceMode2D.Impulse);
+            anim.SetTrigger("Bounce");
         }
     }
     
